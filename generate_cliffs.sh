983608077e096769d5401d48753a50b0d405d634
root <root@DESKTOP-P7MDMQR.>
2025-03-28 17:26:06 +0800

    update code

--- generate_cliffs.sh
+++ generate_cliffs.sh
@@ -8,4 +8,4 @@ source activate trans
 export PYTHONUNBUFFERED=1
 # python generate_cliffs.py -s 0.85
 
-python generate_cliffs.py -d ./data/grampa_e_coli_7_25.csv -c "blosum62 average" -f 2 -s 0.85
+python generate_cliffs.py -d ./data/grampa_e_coli_7_25.csv -c "blosum62 average" -f 2 -t 0.85
